b11d7bb5ca191b74581be5e0b990f47655f8c75d
Mariusz Miłejko <mariusz@milejko.pl>
2019-04-03 14:58:22 +0200

    empty plugin

--- src/MmiSentry/App/SentryFrontControllerPlugin.php
+++ src/MmiSentry/App/SentryFrontControllerPlugin.php
@@ -21,7 +21,5 @@ class SentryFrontControllerPlugin extends \Mmi\App\FrontControllerPluginAbstract
      * @param \Mmi\Http\Request $request
      */
     public function preDispatch(\Mmi\Http\Request $request)
-    {
-        echo 'PRED';
-    }
+    { }
 }
