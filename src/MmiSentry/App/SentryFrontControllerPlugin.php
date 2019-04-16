22d55cdfadf251b2349780079a882f435be181bf
Mariusz Miłejko <mariusz@milejko.pl>
2019-04-16 16:44:25 +0200

    ignoring 404 exceptions

--- src/MmiSentry/App/SentryFrontControllerPlugin.php
+++ src/MmiSentry/App/SentryFrontControllerPlugin.php
@@ -10,13 +10,29 @@
 
 namespace MmiSentry\App;
 
-use Mmi\App\FrontController;
-
 /**
  * Mmi Sentry front controller plugin
  */
 class SentryFrontControllerPlugin extends \Mmi\App\FrontControllerPluginAbstract
 {
+
+    //class name consts
+    const CLASS_NAME_NOT_FOUND_EXCEPTION = 'Mmi\Mvc\MvcNotFoundException';
+    const CLASS_NAME_REGISTRY = '\App\Registry';
+
+    //messages consts
+    const MESSAGE_REGISTRY_ERROR = 'Sentry plugin should be run in MMi application context';
+    const MESSAGE_CONFIG_MISSING = 'Missing Sentry configuration';
+    const MESSAGE_CONFIG_INVALID = 'Sentry configuration invalid, must be instance of \MmiSentry\Config\SentryConfig';
+
+    //sentry consts
+    const SENTRY_KEY_DSN = 'dsn';
+    const SENTRY_KEY_ENVIRONMENT = 'environment';
+    const SENTRY_KEY_BEFORE_SEND = 'before_send';
+    const SENTRY_KEY_TYPE = 'type';
+    const SENTRY_AUTH_ID = 'id';
+    const SENTRY_AUTH_EMAIL = 'email';
+    const SENTRY_AUTH_USERNAME = 'username';
 
     /**
      * @var SentryConfig
@@ -26,11 +42,11 @@ class SentryFrontControllerPlugin extends \Mmi\App\FrontControllerPluginAbstract
     public function __construct()
     {
         //outside app context
-        class_exists('\App\Registry') && isset(\App\Registry::$config) or die('Sentry plugin should be run in MMi application context');
+        class_exists(self::CLASS_NAME_REGISTRY) && isset(\App\Registry::$config) or die(self::MESSAGE_REGISTRY_ERROR);
         //configuration missing
-        isset(\App\Registry::$config->sentry) or die('Missing Sentry configuration');
+        isset(\App\Registry::$config->sentry) or die(self::MESSAGE_CONFIG_MISSING);
         //configuration class invalid
-        (\App\Registry::$config->sentry instanceof \MmiSentry\Config\SentryConfig) or die('Sentry configuration invalid, must be instance of \MmiSentry\Config\SentryConfig');
+        (\App\Registry::$config->sentry instanceof \MmiSentry\Config\SentryConfig) or die(self::MESSAGE_CONFIG_INVALID);
         //getting configuration from the registry
         $this->_config = \App\Registry::$config->sentry;
     }
@@ -47,14 +63,27 @@ class SentryFrontControllerPlugin extends \Mmi\App\FrontControllerPluginAbstract
         }
         //sentry initialization
         \Sentry\init([
-            'dsn' => $this->_config->dsn,
+            self::SENTRY_KEY_DSN => $this->_config->dsn,
             //environment - configured, or guessed by config class name
-            'environment' => $this->_config->environment ? $this->_config->environment : substr(get_class(\App\Registry::$config), 10)
+            self::SENTRY_KEY_ENVIRONMENT => $this->_config->environment ? $this->_config->environment : substr(get_class(\App\Registry::$config), 10),
+            //before send event
+            self::SENTRY_KEY_BEFORE_SEND => function (\Sentry\Event $event) {
+                //getting exception list
+                $exceptions = $event->getExceptions();
+                //ignoring not found exception
+                if (isset($exceptions[0][self::SENTRY_KEY_TYPE]) && self::CLASS_NAME_NOT_FOUND_EXCEPTION == $exceptions[0][self::SENTRY_KEY_TYPE]) {
+                    return;
+                }
+                return $event;
+            },
         ]);
         //adding user content
         $this->_addUserContext();
     }
 
+    /**
+     * Adding user context
+     */
     private function _addUserContext()
     {
         //app has no auth object
@@ -65,13 +94,14 @@ class SentryFrontControllerPlugin extends \Mmi\App\FrontControllerPluginAbstract
         if (!\App\Registry::$auth->hasIdentity()) {
             return;
         }
+        //configure scopes
         \Sentry\configureScope(function (\Sentry\State\Scope $scope): void {
+            //user scope
             $scope->setUser([
-                'id' => \App\Registry::$auth->getId(),
-                'email' => \App\Registry::$auth->getEmail(),
-                'username' => \App\Registry::$auth->getUsername(),
+                self::SENTRY_AUTH_ID => \App\Registry::$auth->getId(),
+                self::SENTRY_AUTH_EMAIL => \App\Registry::$auth->getEmail(),
+                self::SENTRY_AUTH_USERNAME => \App\Registry::$auth->getUsername(),
             ]);
         });
-        return $this;
     }
 }
